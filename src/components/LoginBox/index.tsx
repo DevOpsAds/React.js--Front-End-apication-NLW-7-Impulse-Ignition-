afcd554e95f64f537132944cbb0cb5b22d582f6d
DevOpsAds <joaobatistalimajunior.ads@gmail.com>
2022-05-02 19:53:36 -0300

    problema na linha 48

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -2,51 +2,17 @@ import styles from './styles.module.scss'
 import {VscGithubInverted} from 'react-icons/vsc'
 import GirlLeft from "../../assets/baner-girl-left.png"
 import LogoImg from "../../assets/baner-girl-left.png"
-import { useEffect } from 'react'
+import { useContext, useEffect } from 'react'
 import { api } from '../../services/api'
+import { AuthContext } from '../../contexts/auth'
 
-type AuthResponse={
-    token:string;
-    user:{
-        id:string;
-        avatar_url:string;
-        name:string;
-        login:string;
-    }
 
-}
 
 
 
 function LoginBox(){
-    const signInUrl=`https://github.com/login/oauth/authorize?scope=user&client_id=c9e50849daaff86bec58`
-
-
-   async function signIn(githubCode:string){
-      const response= await api.post<AuthResponse>('http://localhost:4000/authenticate',{     
-            code:githubCode,        
-       })
-
-    //    const{ token ,user}=response.data
-
-    //    localStorage.setIten('@dowhile:token',token);
-       console.log(response.data)
-
-   }
-
-
-    useEffect(()=>{
-        const url=window.location.href;
-        const hasGithubCode = url.includes('?code=')
-
-        if (hasGithubCode){
-            const[urlWithoutCode,githubCode]=url.split('?code=')
-            
-            window.history.pushState({}, '' ,urlWithoutCode)
-            console.log(githubCode)
-            signIn(githubCode)
-        }
-    },[])
+    const{signInUrl}=useContext(AuthContext)
+    // console.log(user)
 
     return(
        
